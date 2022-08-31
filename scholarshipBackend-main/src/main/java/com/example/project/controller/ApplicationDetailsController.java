53909ca20b0ee4b42542991e62c7cc4d27c74421
shailvi9 <i.m.shailvi@gmail.com>
2022-08-31 10:31:12 +0530

    first commit

---
+++ scholarshipBackend-main/src/main/java/com/example/project/controller/ApplicationDetailsController.java
@@ -0,0 +1,109 @@
+package com.example.project.controller;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.project.services.ApplicationDetailsService;
+import com.example.project.models.Application_details;
+import com.example.project.models.DocumentDetails;
+import com.example.project.modelsDto.DocDto;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class ApplicationDetailsController {
+	
+	@Autowired
+	ApplicationDetailsService appSer;
+	
+	Application_details app=new Application_details();
+	
+	@GetMapping("/employee/requests")
+	public List<Application_details> getAllRequests()
+	{
+		return (List<Application_details>)appSer.getAllRequests();
+	}
+	
+	@GetMapping("/employee/requests/{id}")
+	public Application_details getAppDetailsByID(@PathVariable Integer id){
+		return appSer.getAppDetailsById(id);
+	}
+	
+	@PutMapping("/employee/requestApprove/{id}")
+	public void requestApprove(@PathVariable Integer id,@RequestBody Application_details appdetails)
+	{
+		appSer.requestApprove(id, appdetails);
+	}
+	
+	@PutMapping("/employee/requestReject/{id}")
+	public void requestReject(@PathVariable Integer id,@RequestBody Application_details appdetails)
+	{
+		appSer.requestReject(id, appdetails);
+	}
+	
+	@PutMapping("/request/assign/{id}")
+	public void assignedTo(@PathVariable Integer id,@RequestBody Application_details assign)
+	{
+		System.out.println("assigning part............"+" "+assign);
+		appSer.assigned(id, assign);
+	}
+	
+	@PutMapping("/request/student")
+	public void updateApp(@RequestBody Application_details assign)
+	{
+		appSer.updateApp(assign);
+	}
+	
+	
+	@PutMapping("/appDetails")
+	public String puttAppDet(@RequestBody Application_details appDet, @RequestParam(value="id" ,required= true) int id) {
+		return appSer.putAppDetails(appDet, id);
+	}
+	
+	@PostMapping("/appDetails")
+	public int postAppDet(@RequestBody Application_details appDet) {
+		return appSer.postAppDetails(appDet);
+	}
+	
+	@DeleteMapping("/appDetails")
+	public String deleteAppDet(@RequestParam(value="id", required = true) int id) {
+		return appSer.deleteAppDetails(id);
+	}
+	
+	@PostMapping("/uploadFile")
+	public int uploadFile(@RequestParam("file") MultipartFile multipartFile,@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName,@RequestParam("postalAddress") String postalAddress,@RequestParam("collegeName") String collegeName,@RequestParam("state") String state,@RequestParam("className") String className, @RequestParam("studentId") int studentId) throws IOException{
+		 return appSer.uploadFile(multipartFile, firstName,lastName,postalAddress,collegeName,state,className,studentId);
+	  }
+	
+	  
+	  @GetMapping("/downloadFromDB")
+		public void downloadFile(@RequestParam(value = "id" , required = true) int  loanId,HttpServletResponse response) throws Exception{
+			response.getOutputStream().write(fileContent(loanId, response));
+		}
+	  
+	  private byte[] fileContent(int appId,HttpServletResponse response) {
+			return appSer.getFile(appId,response);
+		}
+	  
+	  @GetMapping("/fileType")
+		public DocDto fileType(@RequestParam(value = "id", required = true) int appId) {
+			return appSer.fileType(appId);
+		}
+}
---
+++ scholarshipBackend-main/src/main/java/com/example/project/services/ApplicationDetailsService.java
@@ -0,0 +1,153 @@
+package com.example.project.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.project.models.Application_details;
+import com.example.project.models.UserModel;
+import com.example.project.modelsDto.DocDto;
+import com.example.project.repository.ApplicationDetailsRepository;
+
+@Service
+public class ApplicationDetailsService {
+
+	@Autowired
+	ApplicationDetailsRepository appRepo;
+	
+	public List<Application_details> getAllRequests()
+	{
+		return (List<Application_details>)appRepo.findAll();
+	}
+
+	public Application_details getAppDetailsById(Integer id) {
+		return appRepo.findById(id).get();
+	}
+	
+	public void updateApp(Application_details appUpdate)
+	{
+		Integer id=appUpdate.getAppId();
+		Application_details appupdate=appRepo.findById(id).get();
+		appupdate.setFirstName(appUpdate.getFirstName());
+		appupdate.setLastName(appUpdate.getLastName());
+		appupdate.setClassName(appUpdate.getClassName());
+		appupdate.setCollegeName(appUpdate.getCollegeName());
+		appupdate.setPostalAddress(appUpdate.getPostalAddress());
+		appupdate.setState(appUpdate.getState());
+		appRepo.save(appUpdate);
+	}
+	
+	public void requestApprove(Integer id,Application_details appDetails)
+	{
+		Application_details appdetails=appRepo.findById(id).get();
+		appdetails.setStatus("approved");
+		appRepo.save(appdetails);
+	}
+	
+	public void requestReject(Integer id,Application_details appDetails)
+	{
+		Application_details appdetails=appRepo.findById(id).get();
+		appdetails.setStatus("rejected");
+		appRepo.save(appdetails);
+	}
+	
+	public void assigned(Integer appId,Application_details appDetails)
+	{
+		Application_details appdetails=appRepo.findById(appId).get();
+		appdetails.setAssigned(appDetails.getAssigned());
+		appRepo.save(appdetails);
+	}
+	
+	
+	public int postAppDetails(Application_details app) {
+		
+		if(appRepo.save(app)!=null) {
+			return app.getAppId() ;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public String putAppDetails(Application_details app, int id) {
+		Optional<Application_details> result = appRepo.findById(id);
+		Application_details appDetails = result.get();
+		appDetails.setClassName(app.getClassName());
+		appDetails.setCollegeName(app.getCollegeName());
+		appDetails.setFirstName(app.getFirstName());
+		appDetails.setLastName(app.getLastName());
+		
+		appDetails.setPostalAddress(app.getPostalAddress());
+		appDetails.setState(app.getState());
+		
+		
+		
+		if(appRepo.save(appDetails) !=null) {
+			
+			 return "Successfully updated";
+		}
+		else {
+			return "Not updated successfully";
+		}
+	}
+	
+	public String deleteAppDetails(int id) {
+		
+		try {
+			appRepo.deleteById(id);
+			return "Deleted Successfully";
+			
+		}
+		catch(Exception e) {
+			return "Not Deleted";
+		}
+		
+	}
+	
+	public int uploadFile(MultipartFile multipartFile, String firstName,String lastName,String postalAddress,String collegeName, String state, String className,int studentId) throws IOException {
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+		String url = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/downloadFromDB")
+				.path(fileName)
+				.toUriString();
+		
+		Application_details appDetails = new Application_details(fileName, firstName, lastName, postalAddress, collegeName, state,className, url,multipartFile.getBytes());
+		appDetails.setUserModel(new UserModel(studentId));
+		appDetails.setDocumentType("JPEG");
+		appRepo.save(appDetails);
+		
+		
+		String contentType = multipartFile.getContentType();
+
+		return appDetails.getAppId();
+	}
+	
+	public byte[] getFile(int appId,HttpServletResponse request) {
+
+		Application_details doc =  appRepo.findByAppId(appId);
+
+		request.setHeader("Content-Disposition", "attachment; filename=" + doc.getImage());
+		System.out.println("uploadfile working");
+		return doc.getDocument();			
+	}
+	public DocDto fileType(int appId) {
+		Application_details doc =  appRepo.findByAppId(appId);
+		DocDto documentDto = new DocDto();
+	     documentDto.setDocumentType(doc.getDocumentType());
+		documentDto.setImage(doc.getImage());
+		System.out.println("fileType");
+		return documentDto;
+
+	}
+
+
+}
